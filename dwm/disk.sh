0ea8cdd0a0780e80f376a5076d9246c3eb55bd7f
suozg <alex320388@gmail.com>
2026-03-17 10:01:26 +0200

    Додано функціонал скрипту Радіо. Поправлено Help

--- dwm/disk.sh
+++ dwm/disk.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # Використання кореневого розділу (/)
-usage1=$(df -h | awk '$NF == "/" {print "⛁ " $5}')
+usage1=$(df -h | awk '$NF == "/" {print "󰆼 " $5}')
 
 # Використання /tmp
-usage2=$(df -h | awk '$NF == "/tmp" {print " ▒" $5}')
+usage2=$(df -h | awk '$NF == "/tmp" {print " ▤ " $5}')
 
 # Кількість файлів у корзині
 trash_count=$(trash-list 2>/dev/null | wc -l)
 
 if [ "$trash_count" -gt 0 ]; then
-    trash_icon=" 🗑"
+    trash_icon=" 🚮"
     echo "${usage1}${usage2}${trash_icon}"
 else
     echo "${usage1}${usage2}"
