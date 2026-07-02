660923e59b555853589820f048dffa96a698aea8
suozg <alex320388@gmail.com>
2026-07-02 13:37:12 +0300

    Add files via upload

---
+++ nvim-settings/lua/config/lazy.lua
@@ -0,0 +1,31 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+
+    install = {
+        colorscheme = { "habamax" },
+    },
+
+    checker = {
+        enabled = false,
+    },
+
+    rocks = {
+        enabled = false,
+    },
+})
