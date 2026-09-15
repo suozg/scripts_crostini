d4e9195e76db0988e7a57e2a8f3ceb4417865ec5
suozg <alex320388@gmail.com>
2026-09-15 20:55:14 +0300

    Add files via upload

--- dwm/translator.sh
+++ dwm/translator.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
+# перекладач з англійської на укранську і навпаки
 
 text="$(xclip -selection clipboard -o 2>/dev/null)"
 
 [ -z "$text" ] && exit 0
 
-result="$(sdcv -n "$text" 2>/dev/null |
+# Перевіряємо наявність кириличних літер
+if echo "$text" | grep -q -P '[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]'; then
+    # Назва UK -> EN словника (дізнатися точно можна через `sdcv -l`)
+    DICT_NAME="dictd_www.mova.org_slovnyk_uk-en"
+else
+    # Назва EN -> UK словника
+    DICT_NAME="slovnyk_en-uk"
+fi
+
+# Пошук у конкретному словнику
+result="$(sdcv -u "$DICT_NAME" -n "$text" 2>/dev/null |
     sed -E '/^-->/d; /^Found/d; /^Dictionary/d; /^[[:space:]]*$/d' |
     grep -i -v "^${text}$" |
     tr -d '""' |
