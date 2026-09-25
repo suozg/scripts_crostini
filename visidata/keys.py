db66850c3c10ebde0dfb64db18f4d87827a5c1d2
suozg <alex320388@gmail.com>
2026-09-25 08:03:41 +0300

    Create keys.py

---
+++ visidata/keys.py
@@ -0,0 +1,26 @@
+# налаштування клавіш VisiData
+#-------------------------------------------
+from visidata import TableSheet, BaseSheet
+
+# 1. Спочатку видаляємо базові/стандартні прив'язки для цих символів
+TableSheet.unbindkey('.')
+BaseSheet.unbindkey('.')
+
+# 2. Прив'язуємо крапку до пошуку (search-col)
+TableSheet.bindkey('.', 'search-col')
+BaseSheet.bindkey('.', 'search-col')
+
+# 3. Навігація та пошук
+TableSheet.bindkey('о', 'cursor-down')
+TableSheet.bindkey('л', 'cursor-up')
+TableSheet.bindkey('у', 'edit-cell')
+
+BaseSheet.bindkey('т', 'search-next')
+BaseSheet.bindkey('Т', 'search-prev')
+BaseSheet.bindkey('й', 'quit-sheet')
+
+# 4. Копіювання (українська розкладка для y / Y)
+TableSheet.bindkey('н', 'syscopy-row')
+TableSheet.bindkey('Н', 'syscopy-cell')
+BaseSheet.bindkey('н', 'syscopy-row')
+BaseSheet.bindkey('Н', 'syscopy-cell')
